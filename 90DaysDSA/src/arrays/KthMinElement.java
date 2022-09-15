15f10b0414cb90e517753827f1908c912bd807f5
Hemant Thakur <nikhilt919@gmail.com>
2022-09-15 20:20:33 +0530

    Merge branch 'master' of https://github.com/Hemanthakur/90DaysDSA.git

--- 90DaysDSA/src/arrays/KthMinElement.java
+++ 90DaysDSA/src/arrays/KthMinElement.java
@@ -5,16 +5,15 @@ import java.util.Arrays;
 public class KthMinElement {
 
 	public static void main(String[] args) {
-		int [] nums= {1,5,6,7,3,9,2};
+		int [] nums= {1,5,6,7,3,9};
 		int k=4;
 		System.out.println("kth Smallest element is: "+find(nums,k));
 	}
-	static int find(int[] nums,int k){
+	public static int find(int[] nums,int k){
 		int no=0;
 		Arrays.sort(nums);
 		no=nums[k-1];
 		return no;
-		
 	}
 
 }
