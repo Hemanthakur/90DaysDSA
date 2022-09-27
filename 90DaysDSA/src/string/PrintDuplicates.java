2680d923cab310c84306e62ba76260c5ab5b1704
Hemant Thakur <nikhilt919@gmail.com>
2022-09-27 22:22:49 +0530

    printduplicate

---
+++ 90DaysDSA/src/string/PrintDuplicates.java
@@ -0,0 +1,28 @@
+package string;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class PrintDuplicates {
+	 static void printDups(String str)
+	  {
+	    Map<Character, Integer> count = new HashMap<>();
+	    for (int i = 0; i < str.length(); i++) {
+	      if(count.containsKey(str.charAt(i)))
+	        count.put(str.charAt(i) , count.get(str.charAt(i))+1);
+	      else count.put(str.charAt(i),1);
+	    }
+	 
+	    for (Entry<Character,Integer> countdup : count.entrySet()) { 
+	      if (countdup.getValue() > 1) 
+	        System.out.println(countdup.getKey() + ", count = " + countdup.getValue());
+	    }
+	  }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 String str = "test string";
+		    printDups(str);
+	}
+
+}
