31ded6e01d965cbce37fef41a97c0d3cdfa4c2f5
Hemant Thakur <nikhilt919@gmail.com>
2022-09-27 22:25:21 +0530

    with Map and String

--- 90DaysDSA/src/string/PrintDuplicates.java
+++ 90DaysDSA/src/string/PrintDuplicates.java
@@ -5,24 +5,38 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 public class PrintDuplicates {
-	 static void printDups(String str)
-	  {
-	    Map<Character, Integer> count = new HashMap<>();
-	    for (int i = 0; i < str.length(); i++) {
-	      if(count.containsKey(str.charAt(i)))
-	        count.put(str.charAt(i) , count.get(str.charAt(i))+1);
-	      else count.put(str.charAt(i),1);
-	    }
-	 
-	    for (Entry<Character,Integer> countdup : count.entrySet()) { 
-	      if (countdup.getValue() > 1) 
-	        System.out.println(countdup.getKey() + ", count = " + countdup.getValue());
-	    }
-	  }
+	static void printDups(String str) {
+		Map<Character, Integer> count = new HashMap<>();
+		for (int i = 0; i < str.length(); i++) {
+			if (count.containsKey(str.charAt(i)))
+				count.put(str.charAt(i), count.get(str.charAt(i)) + 1);
+			else
+				count.put(str.charAt(i), 1);
+		}
+
+		for (Entry<Character, Integer> countdup : count.entrySet()) {
+			if (countdup.getValue() > 1)
+				System.out.println(countdup.getKey() + ", count = " + countdup.getValue());
+		}
+	}
+/*
+	static void fillCharCounts(String str, int[] count) {
+		for (int i = 0; i < str.length(); i++)
+			count[str.charAt(i)]++;
+	}
+	static void printDups(String s) {
+		int count[] = new int[26];
+		fillCharCounts(str, count);
+
+		for (int i = 0; i < NO_OF_CHARS; i++)
+			if (count[i] > 1)
+				System.out.println((char) (i) + ", count = " + count[i]);
+	}
+*/
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 String str = "test string";
-		    printDups(str);
+		String str = "test string";
+		printDups(str);
 	}
 
 }
