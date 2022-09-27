e54578383cf897aeba4141c99501698fd8d3d1a9
Hemant Thakur <nikhilt919@gmail.com>
2022-09-27 22:18:35 +0530

    22

--- 90DaysDSA/src/string/ConvertChartoMobileNumber.java
+++ 90DaysDSA/src/string/ConvertChartoMobileNumber.java
@@ -3,7 +3,6 @@ package string;
 public class ConvertChartoMobileNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		String str[] = { "2", "22", "222", "3", "33", "333", "4", "44", "444", "5", "55", "555", "6", "66", "666", "7",
 				"77", "777", "7777", "8", "88", "888", "9", "99", "999", "9999" };
 
