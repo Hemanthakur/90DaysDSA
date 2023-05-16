f197af202036c25b06d8b950432f00199ff19f12
Hemant <108360260+Hemanthakur@users.noreply.github.com>
2023-05-16 11:59:10 +0530

    Create pattern3.java

---
+++ 90DaysDSA/src/Pattern/pattern3.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class MyClass {
+    public static void main(String args[]) {
+        Scanner scn = new Scanner(System.in);
+        int n=scn.nextInt();
+        int sp=n-1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=sp;j++){
+                System.out.print("  ");
+            }
+            sp--;
+            for(int k=1;k<=i;k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+/*
+Output -:
+        * 
+      * * 
+    * * * 
+  * * * * 
+* * * * *    */
